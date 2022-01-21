9de1cd99b33e7f386d21617d3752e47265e57c08
vishwa89 <vishwanath89@gmail.com>
2022-01-22 00:52:21 +0530

    Update ory-donate.js

--- ory-donate.js
+++ ory-donate.js
@@ -18,10 +18,7 @@
 
                     $("#bboxdonation_recurrence_lblRecurringGift").html("Monthly Donation");
                  
-                    $('#lblRecurrenceNextGiftDate').html(function(index,html){
-                            return html.replace("gift","monthly donation");
-                            });
-                    console.log("hi");
+                   
                     
                     
                     if(flag_url == "" || flag_url.indexOf("") >= 0 ){
@@ -284,10 +281,16 @@
            if(($('.single-donation-btn').hasClass('monthly-not-active'))){
             $('.single-donation-btn').removeClass('monthly-not-active');
              $('#bboxdonation_designation_divSection').hide();
+               $('#lblRecurrenceNextGiftDate').hide();
                setTimeout(
                     function() 
                         {
-                            console.log($('#lblRecurrenceNextGiftDate').html());
+                           
+                             $('#lblRecurrenceNextGiftDate').html(function(index,html){
+                            return html.replace("gift","monthly donation");
+                            });
+                    console.log("h");
+                            $('#lblRecurrenceNextGiftDate').show();
                     }, 2000);
             
             }
