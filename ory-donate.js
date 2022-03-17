382b7fce61a4dba77bcf83e8c2db1385be5d5a05
vishwa89 <vishwanath89@gmail.com>
2022-03-17 15:46:36 +0530

    Update ory-donate.js

--- ory-donate.js
+++ ory-donate.js
@@ -2,7 +2,7 @@
         var flag =true,
             $fname,$lnam,$email,$address,$city,$state,$postcode,$pCategory,$pName,$pSku,$revenue,$monthly_exist,$flag_designation_show = false,$pSkudata,$pAmtdata,$pCategorydata;
             
-            console.log("he");
+            console.log("h");
 
 
 
