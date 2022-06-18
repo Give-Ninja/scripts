24dd038f59e5cf855b55236643bd5ca70c9d684b
vishwa89 <vishwanath89@gmail.com>
2022-06-18 22:02:40 +0530

    Update hamlin.js

--- hamlin.js
+++ hamlin.js
@@ -1,4 +1,4 @@
-    jquery(document).ready(function() {
+    jquery(document).ready(function($) {
         var flag =true,
             $fname,$lnam,$email,$address,$city,$state,$postcode,$pCategory,$pName,$pSku,$revenue,$monthly_exist,$flag_designation_show = false,$pSkudata,$pAmtdata,$pCategorydata;
             
