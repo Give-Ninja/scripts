a10b41882d604d7758e94e4df0759d821be179af
vishwa89 <vishwanath89@gmail.com>
2022-06-20 15:56:52 +0530

    Update hamlin.js

--- hamlin.js
+++ hamlin.js
@@ -1,4 +1,4 @@
- jquery(document).ready(function() {
+(function($) {
         var flag =true,
             $fname,$lnam,$email,$address,$city,$state,$postcode,$pCategory,$pName,$pSku,$revenue,$monthly_exist,$flag_designation_show = false,$pSkudata,$pAmtdata,$pCategorydata;
             
@@ -619,6 +619,5 @@ function elementLoaded(el, cb) {
         }
         
       
-     });
-    
+    })( jQuery );
     
