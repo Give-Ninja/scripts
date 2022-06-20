8d6df3e9e8769c118b5fc1d3b3867ba6dae8ee4b
vishwa89 <vishwanath89@gmail.com>
2022-06-20 22:53:31 +0530

    Update hamlin.js

--- hamlin.js
+++ hamlin.js
@@ -579,7 +579,7 @@ function elementLoaded(el, cb) {
 
                 $('.form-container .go-back').hide();
                     $('.form-container .button-wrapper .button--orange,.BBDFormSectionGiftInfo,.BBFormSectionRecurrenceInfo,.BBFormProgressContainer,.BBDFormSectionTributeInfo').show();
-                    $('.BBDFormSectionBillingInfo,.BBFormButtonRow,.BBDFormSectionPaymentInfo,.BBFormErrorBlock,.BBFieldBillingStateZip').hide();
+                    $('.BBDFormSectionBillingInfo,.BBFormButtonRow,.BBDFormSectionPaymentInfo,.BBFormErrorBlock,.BBFieldBillingStateZip .BBFormFieldContainer').hide();
                        
                          if($('#bboxdonation_tribute_chkTributeGift').length){
                        if($('#bboxdonation_tribute_chkTributeGift')[0].checked){
@@ -599,7 +599,7 @@ function elementLoaded(el, cb) {
                 else if( prev.index() == 1 ) {
 
                     $('.BBDFormSectionGiftInfo,.BBFormSectionRecurrenceInfo,.BBFormButtonRow,.BBDFormSectionPaymentInfo,.BBFormErrorBlock,.BBFormProgressContainer,.BBDFormSectionTributeInfo,.BBDFormSectionComments').hide();
-                    $('.form-container .button-wrapper .button--orange,.BBDFormSectionBillingInfo,.BBDFormSectionBillingInfo .BBFormBillingPhone,.BBFieldBillingStateZip').show();
+                    $('.form-container .button-wrapper .button--orange,.BBDFormSectionBillingInfo .BBFormBillingPhone,.BBFieldBillingStateZip .BBFormFieldContainer').show();
                 }
             }
             
